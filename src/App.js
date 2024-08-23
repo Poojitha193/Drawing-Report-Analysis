b0dd10d60755327902466017bb678d003b0e2d89
Poojitha193 <poojitha.pulluri03@gmail.com>
2024-08-23 23:48:48 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import DrawingReportAnalysis from './components/DrawingReportAnalysis';
+
+function App() {
+
+  
+  return (
+
+    <div className="bg-slate-100">
+    <DrawingReportAnalysis/>
+    </div>
+  );
+}
+
+export default App;
